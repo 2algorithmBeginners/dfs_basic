11ffb5628e5eef0f65b242b3c7e54562c324bd34
parkchansoo <chansoo1563@gmail.com>
2018-07-31 16:11:49 +0900

    commemts to easy understand

--- dfs_maze/src/Main.java
+++ dfs_maze/src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
+
+    /*
+     * our game marker can only moves up, left, down, and right.
+     * let's assign those moves in two-dimensional array.
+     * with this array, we can easily iterate the implementation for each moves
+     */
     public static int[][] ways = {
         {-1, 0},
         {0, -1},
@@ -19,6 +25,11 @@ public class Main {
         );
     }
 
+    /*
+     * recursive method for dfs.
+     * our marker makes recursive moves.
+     * if there is place which marker can move, we do next recursion on next place.
+     */
     public static int maze_dfs(int [][] array, int x, int y, int answer) {
         printList(new int[] {x, y});
         int ret = -1;
@@ -28,17 +39,23 @@ public class Main {
         }
 
         int tmp;
-        int xtmp;
-        int ytmp;
+        int xtmp; // To assign next step
+        int ytmp; // To assign next step
+        /*
+         * you may use 4 step of if & else to watch all possible step!
+         * but we already assigned ways. and the codes are exactly same.
+         * by using for-loop we can make code more DRY.
+         */
         for(int i = 0; i < ways.length; i ++) {
             xtmp = x + ways[i][0];
             ytmp = y + ways[i][1];
 
             if(xtmp > -1 && xtmp < array.length && ytmp > -1 && ytmp < array[0].length){
                 if(array[xtmp][ytmp] == 1) {
-                    array[x][y] = 0;
+                    array[x][y] = 0; // to avoid returning step which causes infinite loop.
                     tmp = maze_dfs(array, xtmp, ytmp, answer);
-                    array[x][y] = 1;
+                    array[x][y] = 1; // after travel we need to recover for using next iteration.
+                    // if we found the way to out. just return answer!
                     if(tmp != -1)
                         return tmp;
                 }
